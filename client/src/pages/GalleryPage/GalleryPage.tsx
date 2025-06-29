b90f86da1ccd559d8e417d431f3dd5800b819cbb
G-Ludovic <l.galicher@free.fr>
2025-06-29 16:03:04 +0200

    resolved import format vite.config.ts

--- client/src/pages/GalleryPage/GalleryPage.tsx
+++ client/src/pages/GalleryPage/GalleryPage.tsx
@@ -1,9 +1,31 @@
 import "./GalleryPage.css";
+import { drawings } from "../../data/drawings.ts";
 
 function GalleryPage() {
   return (
     <>
       <h1>Galerie</h1>
+      <main className="gallery-page">
+        <article className="gallery-article">
+          {drawings.map((drawing) => (
+            <div key={drawing.id} className="card">
+              <a
+                className="click-link"
+                href={drawing.art}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="draw-img"
+                  src={drawing.art}
+                  alt={drawing.name || `Dessin #${drawing.id}`}
+                />
+              </a>
+              <p>{drawing.name || `Dessin #${drawing.id}`}</p>
+            </div>
+          ))}
+        </article>
+      </main>
     </>
   );
 }
