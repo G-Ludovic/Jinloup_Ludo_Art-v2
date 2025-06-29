ef0d2b20a841cc0a0c08b6712d08e4a2526738e4
G-Ludovic <l.galicher@free.fr>
2025-06-29 14:04:28 +0200

    database first init

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,10 @@
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
-  password varchar(255) not null
-);
-
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+  password varchar(500) not null
 );
 
 insert into user(id, email, password)
 values
   (1, "jdoe@mail.com", "123456");
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
