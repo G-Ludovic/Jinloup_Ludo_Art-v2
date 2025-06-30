58783b91567c58a36ce96c31faf5381058101d6d
G-Ludovic <l.galicher@free.fr>
2025-06-30 08:48:53 +0200

    work in progress

---
+++ client/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,35 @@
+import "./RegistrationPage.css";
+
+function RegistrationPage() {
+  return (
+    <>
+      <h1>Enregistrement</h1>
+      <main className="registration-page">
+        <form>
+          <label htmlFor="email">Votre email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Ex: loup@gmail.com"
+          />
+
+          <label htmlFor="password">Votre mot de passe</label>
+          <input type="password" name="password" placeholder="************" />
+
+          <label htmlFor="confirm-password">Confirmez votre mot de passe</label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            placeholder="************"
+          />
+
+          <button type="button">Valider</button>
+        </form>
+      </main>
+    </>
+  );
+}
+
+export default RegistrationPage;
