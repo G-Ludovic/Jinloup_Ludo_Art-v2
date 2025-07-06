bf60ef2c0cbaa923a252bb87e70260eb2b23952b
G-Ludovic <l.galicher@free.fr>
2025-07-06 14:10:33 +0200

    create req post user

---
+++ server/src/modules/user/userActions.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler } from "express";
+import userRepository from "./userRepository";
+
+const add: RequestHandler = async (req, res) => {
+  try {
+    const user = await userRepository.create(req.body);
+
+    if (user) {
+      res
+        .status(201)
+        .json("Congratulations, your account has been created successfully !");
+    } else {
+      res.status(404).json("An error occured during the registration");
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+};
+
+export default { add };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,18 +1,8 @@
 import express from "express";
+import userActions from "./modules/user/userActions";
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Define item-related routes
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
-/* ************************************************************************* */
+router.post("/user", userActions.add);
 
 export default router;
