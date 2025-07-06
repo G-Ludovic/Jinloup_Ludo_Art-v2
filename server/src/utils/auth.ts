bfafeafe354b7a2367efc6a056517b6a462631c5
G-Ludovic <l.galicher@free.fr>
2025-07-06 17:55:32 +0200

    creation of the JWT token

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,6 +1,7 @@
 import argon2 from "argon2";
 import type { RequestHandler } from "express";
 import userRepository from "../modules/user/userRepository";
+import jwt from "jsonwebtoken";
 
 const hashPassword: RequestHandler = async (req, res, next) => {
   try {
@@ -24,11 +25,15 @@ const login: RequestHandler = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // 1ere partie
+
     const user = await userRepository.readByEmail(email);
 
     if (!user) {
       throw new Error("This user doesn't exist");
     }
+
+    // 2ème partie
 
     const isPasswordValid = await argon2.verify(user.password, password);
 
@@ -36,7 +41,24 @@ const login: RequestHandler = async (req, res) => {
       throw new Error("Invalid password");
     }
 
-    res.status(200).json("Congratulations, you're logged in !");
+    // 3ème partie
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const secretKey = process.env.APP_SECRET;
+
+    if (!secretKey) {
+      throw new Error("A secret must be provided");
+    }
+
+    const token = jwt.sign(payload, secretKey);
+
+    res
+      .status(200)
+      .json({ message: "Congratulations, you're logged in !", token });
   } catch (err) {
     console.warn((err as Error).message);
     res.sendStatus(500);
