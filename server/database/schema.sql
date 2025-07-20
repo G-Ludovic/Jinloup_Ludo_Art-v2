f02a385631d622330d487b0c4585119410cc973e
G-Ludovic <l.galicher@free.fr>
2025-07-20 13:53:17 +0200

    update database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE user (
 
 INSERT INTO user (id, email, password)
 VALUES
-  (1, "johndoe@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$bnh3SWdQcXZOVXc5SEF4NQ$HhwqNmpIzASIlOYXi1mS4g");
-
+  (1, "johndoe@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$WmRCS21yeTVMSGFlYkFiSA$BD49Ws1JoGxpCr0lF5Mkxw"),
+  (2, "petitloup@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$NFpuWXFyYldKZTVXaDd3Yw$sEPJMkW3cOEiBNgK6rQOSg"),
+  (3, "kitsunekiss@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$Rmg2MnVtaGd5dG1xdGtmbA$cmHBOEOuGivPybVLLMEAwg");
