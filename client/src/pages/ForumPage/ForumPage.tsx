8a895d4d5d2ca16468c0febe6d48fab070ea430b
G-Ludovic <l.galicher@free.fr>
2025-07-20 22:11:08 +0200

    css editing

--- client/src/pages/ForumPage/ForumPage.tsx
+++ client/src/pages/ForumPage/ForumPage.tsx
@@ -170,33 +170,42 @@ function ForumPage() {
         </article>
         <article className="onligne-forum">
           <h3>Qui est en ligne ?</h3>
-          <tbody>
-            <tr>
-              <th scope="row">logo 1</th>
-              <td>Admin</td>
-              <td>: Nb</td>
-            </tr>
-            <tr>
-              <th scope="row">logo 2</th>
-              <td>Modérateur</td>
-              <td>: Nb</td>
-            </tr>
-            <tr>
-              <th scope="row">logo 3</th>
-              <td>Grand Loup</td>
-              <td>: Nb</td>
-            </tr>
-            <tr>
-              <th scope="row">logo 4</th>
-              <td>Loup</td>
-              <td>: Nb</td>
-            </tr>
-            <tr>
-              <th scope="row">logo 5</th>
-              <td>Louveteau</td>
-              <td>: Nb</td>
-            </tr>
-          </tbody>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Indicateur</th>
+                <th scope="col">Grades</th>
+                <th scope="col">En ligne</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">logo 1</th>
+                <td>Admin</td>
+                <td>: Nb</td>
+              </tr>
+              <tr>
+                <th scope="row">logo 2</th>
+                <td>Modérateur</td>
+                <td>: Nb</td>
+              </tr>
+              <tr>
+                <th scope="row">logo 3</th>
+                <td>Grand Loup</td>
+                <td>: Nb</td>
+              </tr>
+              <tr>
+                <th scope="row">logo 4</th>
+                <td>Loup</td>
+                <td>: Nb</td>
+              </tr>
+              <tr>
+                <th scope="row">logo 5</th>
+                <td>Louveteau</td>
+                <td>: Nb</td>
+              </tr>
+            </tbody>
+          </table>
         </article>
         <article className="regulation-forum">
           <h3>Règlement</h3>
