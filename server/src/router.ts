937fd062365d64fcd58b2cd67e6b8a94561b8b8c
G-Ludovic <l.galicher@free.fr>
2025-07-22 00:04:35 +0200

    Setting up multer, localstorage, adding, deleting and modifying. Css update

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import drawActions from "./modules/draw/drawActions";
 import userActions from "./modules/user/userActions";
 import auth from "./utils/auth";
+import files from "./utils/files";
 import validation from "./utils/validation";
 
 const router = express.Router();
@@ -22,7 +23,7 @@ router.get("/refresh", auth.refreshToken);
 router.get("/draws", drawActions.browse);
 router.get("/draws/:id", drawActions.read);
 router.put("/draws/:id", drawActions.edit);
-router.post("/draws", drawActions.add);
+router.post("/draws", files.imageUpload, files.drawImage, drawActions.add);
 router.delete("/draws/:id", drawActions.destroy);
 
 export default router;
---
+++ server/src/utils/files.ts
@@ -0,0 +1,41 @@
+import crypto from "node:crypto";
+import path from "node:path";
+import type { RequestHandler } from "express";
+import multer from "multer";
+
+// 1. Création du diskStorage
+//   --> Déclarer la destination du fichier
+//   --> Renommer le fichier
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "public/uploads/");
+  },
+
+  filename(req, file, callback) {
+    const base = path.basename(file.originalname).toLowerCase();
+    const ext = path.extname(file.originalname).toLowerCase();
+    const id = crypto.randomUUID();
+    callback(null, `${base}_${id}${ext}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const imageUpload = upload.single("image");
+
+const drawImage: RequestHandler = (req, res, next) => {
+  try {
+    if (req.file) {
+      req.body.image = `/uploads/${req.file.filename}`;
+
+      next();
+    } else {
+      res.sendStatus(400).json("File problem");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { imageUpload, drawImage };
