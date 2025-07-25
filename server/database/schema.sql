5db17d6db538c14d97e2281b75ead98697d8dc03
G-Ludovic <l.galicher@free.fr>
2025-07-25 12:03:07 +0200

    work in progress

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,12 @@
 CREATE TABLE user (
-  ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  pseudo VARCHAR(50) NOT NULL,
+  avatar VARCHAR(255),
+  location VARCHAR(100),
+  bio TEXT,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(500) NOT NULL
+  password VARCHAR(500) NOT NULL,
+  registration_date DATE NOT NULL
 );
 
 CREATE TABLE draw (
@@ -10,12 +15,40 @@ CREATE TABLE draw (
   image VARCHAR(250)
 );
 
-INSERT INTO user (id, email, password)
-VALUES
-  (1, "johndoe@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$WmRCS21yeTVMSGFlYkFiSA$BD49Ws1JoGxpCr0lF5Mkxw"),
-  (2, "petitloup@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$NFpuWXFyYldKZTVXaDd3Yw$sEPJMkW3cOEiBNgK6rQOSg"),
-  (3, "jinshi.wolf@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$cEdpRHRQdnR1YzZCQ05tVg$STyFmxsSPaX2akYNWNEZ9A"),
-  (4, "kitsunekiss@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$Rmg2MnVtaGd5dG1xdGtmbA$cmHBOEOuGivPybVLLMEAwg");
+CREATE TABLE comment (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  draw_date DATE NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  draw_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (draw_id) REFERENCES draw(id)
+);
+
+CREATE TABLE subject (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  category VARCHAR(50),
+  creation_date DATE NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+CREATE TABLE message (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  content TEXT NOT NULL,
+  sending_date DATE NOT NULL,
+  user_id INT NOT NULL,
+  subject_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (subject_id) REFERENCES Subject(id)
+);
+
+-- INSERT INTO user (id, pseudo, avatar, location, bio, email, password)
+-- VALUES
+--   (1, "Jdoe", "###", "Lost", "Je suis un fantôme", "johndoe@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$WmRCS21yeTVMSGFlYkFiSA$BD49Ws1JoGxpCr0lF5Mkxw"),
+--   (2, "PetitLoup", "###", "Toundra", "", "petitloup@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$NFpuWXFyYldKZTVXaDd3Yw$sEPJMkW3cOEiBNgK6rQOSg"),
+--   (3, "Jinloup", "jinshi.wolf@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$cEdpRHRQdnR1YzZCQ05tVg$STyFmxsSPaX2akYNWNEZ9A"),
+--   (4, "Kitsune", "kitsunekiss@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$Rmg2MnVtaGd5dG1xdGtmbA$cmHBOEOuGivPybVLLMEAwg");
 
 -- INSERT INTO draw (id, name, image)
 -- VALUES
