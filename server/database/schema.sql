6843df13f44752f9bb40e811d54a7e6f63ac42e2
G-Ludovic <l.galicher@free.fr>
2025-12-10 10:54:17 +0100

    my users update fixbug

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -63,12 +63,12 @@ CREATE TABLE comment (
 );
 
 -- INSERT USER DE DÉMO
-INSERT INTO user (pseudo, avatar, location, bio, email, password)
+INSERT INTO user (id, pseudo, avatar, location, bio, email, password, registration_date)
 VALUES
-('Jdoe', '###', '2, rue fantôme, 99 999 Lost', 'Je suis un fantôme', 'johndoe@gmail.com', 'dummy1'),
-('PetitLoup', '###', '12, run de la neige, 59 999 Toundra', 'Je suis un petit loup mignon', 'petitloup@gmail.com', 'dummy2'),
-('Jinloup', '###', '25, rue de la montagne, 59 999 Toundra', 'Le secret du loup', 'jinshi.wolf@gmail.com', 'dummy3'),
-('Kitsune', '###', '58, rue de la renardière, 79 999 Fox', 'Le secret de la renarde', 'kitsunekiss@gmail.com', 'dummy4');
+  (1, "Jdoe", "###", "2, rue fantôme, 99 999 Lost", "Je suis un fantôme", "johndoe@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$WmRCS21yeTVMSGFlYkFiSA$BD49Ws1JoGxpCr0lF5Mkxw", "2025-05-25"),
+  (2, "PetitLoup", "###", "12, run de la neige, 59 999 Toundra", "Je suis un petit loup mignon", "petitloup@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$NFpuWXFyYldKZTVXaDd3Yw$sEPJMkW3cOEiBNgK6rQOSg", "2024-06-14"),
+  (3, "Jinloup", "###", "25, rue de la montagne, 59 999 Toundra", "Le secret du loup", "jinshi.wolf@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$cEdpRHRQdnR1YzZCQ05tVg$STyFmxsSPaX2akYNWNEZ9A", "2020-01-22"),
+  (4, "Kitsune", "###", "58, rue de la renardière, 79 999 Fox", "Le secret de la renarde", "kitsunekiss@gmail.com", "$argon2id$v=19$m=16,t=2,p=1$Rmg2MnVtaGd5dG1xdGtmbA$cmHBOEOuGivPybVLLMEAwg", "2025-01-06");
 
 -- INSERT CATEGORY (forum principal)
 INSERT INTO category (name, description)
