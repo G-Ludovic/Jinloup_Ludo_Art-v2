fb90478682beff050f4aecc861f020449187f809
G-Ludovic <l.galicher@free.fr>
2025-12-16 13:03:43 +0100

    work in progress

--- client/src/categories/YourCreations/YourCreations.tsx
+++ client/src/categories/YourCreations/YourCreations.tsx
@@ -1,21 +1,33 @@
 import CategoryTemplate from "../../components/CategoryTemplate/CategoryTemplate";
+import OnlineForum from "../../components/OnlineForum/OnlineForum";
 import "./YourCreations.css";
 
 function YourCreations() {
   return (
-    <main>
+    <>
       <header className="category-section-forum">
         <h2>Vos créations</h2>
         <p>Partage tes dessins</p>
       </header>
 
-      <CategoryTemplate
-        title="Vos créations"
-        description="Partage ici tes dessins, vidéos, musiques et autres œuvres !"
-        subjectId={3}
-        userId={3}
-      />
-    </main>
+      <main className="presentations-layout">
+        <section className="forum-content">
+          <article className="posts-forum">
+            <CategoryTemplate
+              title="Vos créations"
+              description="Partage ici tes dessins, vidéos, musiques et autres œuvres !"
+              subjectId={3}
+              userId={3}
+            />
+          </article>
+
+          <article className="visual-forum">
+            <img src="/images/bc_online_latests.webp" alt="wolfman warrior" />
+            <OnlineForum />
+          </article>
+        </section>
+      </main>
+    </>
   );
 }
 
