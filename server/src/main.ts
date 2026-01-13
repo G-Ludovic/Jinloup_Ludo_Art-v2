644e0d8b2ce297cafaa557efce31f4af625c1f4d
G-Ludovic <l.galicher@free.fr>
2026-01-13 19:37:49 +0100

    fix: bug x2 URL

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import "../database/checkConnection";
 import app from "./app";
 
 // Définir le port à utiliser
-const port = process.env.APP_PORT || 3310;
+const port = process.env.PORT || process.env.APP_PORT || 3310;
 
 app
   .listen(port, () => {
